4f0ddfd7870ec65c3001ace5755c8b4abbda4462
Andy <andy.10lin@hotmail.com>
2026-06-11 16:16:05 -0400

    Add rotational velocity when turning and calculate slip angles

--- src/game/car.py
+++ src/game/car.py
@@ -10,13 +10,16 @@ class Car:
     self.angle_deg = angle_deg
 
     self.vel = pr.Vector2(0, 0)
+    self.angular_velocity = 0.0
+    self.max_lateral_accel = 10.0
 
-    self.wheelbase = 40.0
+    self.wheelbase = 60.0
     self.max_steer = 50.0
     self.grip = 5.0
+    self.cornering_stiffness = 10.0
 
-    self.engine_power = 1000.0
-    self.brake_power = 1200.0
+    self.engine_power = 800.0
+    self.brake_power = 600.0
     self.engine_brake = 20.0
     self.air_resist = 0.3
 
@@ -31,11 +34,17 @@ class Car:
     forward_speed = pr.vector2_dot_product(self.vel, forward)
 
     steer_angle = math.radians(self.max_steer) * steer
-    yaw_rate = forward_speed / self.wheelbase * math.tan(steer_angle)
+    target_yaw_rate = forward_speed / self.wheelbase * math.tan(steer_angle)
 
-    self.angle_deg += yaw_rate * dt
+    yaw_response = 1
+    self.angular_velocity += (
+      (target_yaw_rate - self.angular_velocity) * yaw_response * dt
+    )
+
+    self.angle_deg += math.degrees(self.angular_velocity) * dt
 
   def update_physics(self, dt: float, throttle: bool, brake: bool):
+    tire_fric = 2
     angle_rad = math.radians(self.angle_deg)
     forward = pr.Vector2(math.sin(angle_rad), -math.cos(angle_rad))
     right = pr.Vector2(math.cos(angle_rad), math.sin(angle_rad))
@@ -44,8 +53,10 @@ class Car:
 
     if throttle and brake:
       forward_speed -= self.brake_power * dt
+      tire_fric = 5.0
     elif brake:
       forward_speed -= self.brake_power * dt
+      tire_fric = 5.0
     elif throttle:
       forward_speed += self.engine_power * dt
     else:
@@ -53,21 +64,29 @@ class Car:
 
     forward_speed -= self.air_resist * forward_speed * dt
     lateral_speed -= self.air_resist * lateral_speed * dt
-    lateral_speed *= max(0.0, 1.0 - self.grip * dt)
+
+    lateral_speed -= lateral_speed * tire_fric * dt
 
     if forward_speed < 0:
       forward_speed = 0
+
+    slip_angle = math.atan2(lateral_speed, max(abs(forward_speed), 1.0))
+    lateral_force = -slip_angle * self.cornering_stiffness
+    lateral_force = pr.clamp(
+      lateral_force, -self.max_lateral_accel, self.max_lateral_accel
+    )
+
+    lateral_speed += lateral_force * dt
 
     self.vel.x = forward.x * forward_speed + right.x * lateral_speed
     self.vel.y = forward.y * forward_speed + right.y * lateral_speed
 
     speed = pr.vector2_length(self.vel)
-    slip_angle = math.atan2(lateral_speed, abs(forward_speed))
 
     print(
       f"{[f'{self.vel.x:<20.5f}', f'{self.vel.y:<20.5f}']} {
         [f'{speed:<20.5f}', f'{forward_speed:<20.5f}', f'{lateral_speed:<20.5f}']
-      } {slip_angle}"
+      } {math.degrees(slip_angle):<20.5f} {self.angular_velocity:<20.5f}"
     )
 
   def update_pos(self, dt: float):
