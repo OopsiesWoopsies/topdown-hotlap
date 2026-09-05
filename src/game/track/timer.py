c29576580d1fbac2a07ada9bb4c0ff0377cadcda
Andy <andy.10lin@hotmail.com>
2026-09-05 18:40:34 -0400

    Change the timing system from system clock to dt based for ML training and anti-cheat
    Implement anti-cheat by comparing real time to simulation time
    
    Current lap now continues when car goes off and clock turns red indicating an invalid lap

--- src/game/track/timer.py
+++ src/game/track/timer.py
@@ -1,7 +1,7 @@
 import time
 
 
-def ms_to_str(ms: int, lap_time: bool = False) -> str:
+def sec_to_str(sec: float, is_lap_time: bool = False) -> str:
   """Convert seconds to a readable time, MM:SS.MS.
 
   Args:
@@ -10,71 +10,87 @@ def ms_to_str(ms: int, lap_time: bool = False) -> str:
   Returns:
     str: A string that reads: MM:SS.MS or SS.MS (if MM is 0), where minutes does not convert to hours and milliseconds reads up to the thousandths place.
   """
-  seconds = ms // 1000
-  ms %= 1000
-  minutes = seconds // 60
-  seconds %= 60
-  if minutes == 0 and not lap_time:
-    return f"{seconds:02}.{ms:03}"
-  return f"{minutes:02}:{seconds:02}.{ms:03}"
+  ms = int((sec * 1000) % 1000)
+  minutes = int(sec / 60)
+  sec = int(sec % 60)
+  if minutes == 0 and not is_lap_time:
+    return f"{sec:02}.{ms:03}"
+  return f"{minutes:02}:{sec:02}.{ms:03}"
 
 
 class RaceTime:
-  def __init__(self, ms: float, str_format: str):
-    self.ms = ms
+  def __init__(self, sec: float = 0.0, str_format: str = "00:00.000"):
+    self.sec = sec
     self.str_format = str_format
 
-  def update_time(self, ms: float, lap_time: bool = False):
-    self.ms = ms
-    self.str_format = ms_to_str(ms, lap_time)
+  def update_time(self, sec: float, is_lap_time: bool = False):
+    self.sec = sec
+    self.str_format = sec_to_str(sec, is_lap_time)
 
 
 class Timer:
-  def __init__(self):
-    # All in ms unless stated otherwise
-    self.start_lap_time = 0.0  # s
-    self.start_sector_time = 0.0  # s
-
-    self.sector_times = [RaceTime(0, "00.000") for _ in range(3)]
-    self.prev_sector_times = [RaceTime(0, "00.000") for _ in range(3)]
+  def __init__(self, is_human: bool):
+    self.sector_times = [RaceTime(0.0, "00.000") for _ in range(3)]
+    self.prev_sector_times = [RaceTime(0.0, "00.000") for _ in range(3)]
     self.best_sector_times = [
       RaceTime(float("inf"), "00.000") for _ in range(3)
     ]  # Get this from storage
 
-    self.curr_lap_time = RaceTime(0, "00:00.000")
-    self.prev_lap_time = RaceTime(0, "00:00.000")
+    self.curr_lap_time = RaceTime()
+    self.curr_sector_time = 0.0
+
+    self.prev_lap_time = RaceTime()
     self.best_lap_time = RaceTime(float("inf"), "00:00.000")  # Get this from storage
 
     self.lap_timer_stopped = True
 
-  def start_lap_timer(self):
-    self.start_lap_time = time.perf_counter()
-    self.start_sector_time = time.perf_counter()
-    self.sector_times = [RaceTime(0, "00.000") for _ in range(3)]
-    self.lap_timer_stopped = False
+    # Lap validation
+    self.is_human = is_human
+    self.valid_lap = True
+    self.world_timer = 0.0
 
-  def get_elapsed_time(self, start_time: float) -> float:
-    return round((time.perf_counter() - start_time) * 1000)
+  def start_lap_timer(self):
+    self.curr_lap_time = RaceTime()
+    self.curr_sector_time = 0.0
+    self.sector_times = [RaceTime(0.0, "00.000") for _ in range(3)]
+    self.lap_timer_stopped = False
+    self.valid_lap = True
+
+    if self.is_human:
+      self.world_timer = time.perf_counter()
+
+  def update_timer(self, dt: float):
+    self.curr_lap_time.update_time(self.curr_lap_time.sec + dt, is_lap_time=True)
+    self.curr_sector_time += dt
 
   def stop_lap_timer(self):
-    self.curr_lap_time.update_time(self.get_elapsed_time(self.start_lap_time), True)
     self.lap_timer_stopped = True
 
   def set_lap_time(self):
-    self.prev_lap_time.update_time(self.get_elapsed_time(self.start_lap_time), True)
-    self.start_lap_time = time.perf_counter()
-    if self.best_lap_time.ms > self.prev_lap_time.ms:
-      self.best_lap_time.update_time(self.prev_lap_time.ms, True)
+    self.prev_lap_time = self.curr_lap_time
+    self.curr_lap_time = RaceTime()
+    self.curr_sector_time = 0.0
+    self.valid_lap = True
+
+    if self.is_human:
+      self.world_timer = time.perf_counter()
+      real_time_ratio = (
+        (time.perf_counter() - self.world_timer) * 1000 / self.prev_lap_time.sec
+      )
+      if self.prev_lap_time.sec != 0.0 and real_time_ratio > 1.10:
+        print("lap invalidated for desynced time")  # Draw this
+        self.valid_lap = False
+
+    if self.valid_lap and self.best_lap_time.sec > self.prev_lap_time.sec:
+      self.best_lap_time.update_time(self.prev_lap_time.sec, is_lap_time=True)
 
     for i in range(3):
-      self.prev_sector_times[i].update_time(self.sector_times[i].ms)
-      self.sector_times[i] = RaceTime(0, "00.000")
+      self.prev_sector_times[i].update_time(self.sector_times[i].sec)
+      self.sector_times[i] = RaceTime(0.0, "00.000")
 
   def set_sector_time(self, sector: int):
     sector_i = sector - 1
-    self.sector_times[sector_i].update_time(
-      self.get_elapsed_time(self.start_sector_time)
-    )
-    self.start_sector_time = time.perf_counter()
-    if self.best_sector_times[sector_i].ms > self.sector_times[sector_i].ms:
-      self.best_sector_times[sector_i].update_time(self.sector_times[sector_i].ms)
+    self.sector_times[sector_i].update_time(self.curr_sector_time)
+    self.curr_sector_time = 0.0
+    if self.best_sector_times[sector_i].sec > self.sector_times[sector_i].sec:
+      self.best_sector_times[sector_i].update_time(self.sector_times[sector_i].sec)
--- src/render/render_timer.py
+++ src/render/render_timer.py
@@ -1,6 +1,6 @@
 import pyray as pr
 
-from game.track.timer import Timer, ms_to_str
+from game.track.timer import Timer
 
 
 class RenderTimer:
@@ -10,14 +10,14 @@ class RenderTimer:
     self.background = pr.Rectangle(-10, -10, 300, 240)
 
   def draw(self, x: int, y: int):
-    if self.timer.lap_timer_stopped:
-      total_time = self.timer.curr_lap_time.str_format
-    else:
-      total_time = self.timer.get_elapsed_time(self.timer.start_lap_time)
-      total_time = ms_to_str(round(total_time), True)
-
     pr.draw_rectangle_rounded(self.background, 0.3, 10, (0, 0, 0, 120))
-    pr.draw_text(total_time, x, y, 30, pr.WHITE)
+    pr.draw_text(
+      self.timer.curr_lap_time.str_format,
+      x,
+      y,
+      30,
+      pr.RED if self.timer.lap_timer_stopped else pr.WHITE,
+    )
     pr.draw_text(self.timer.prev_lap_time.str_format, x, y + 70, 20, pr.WHITE)
     pr.draw_text("Best Lap & Sectors", x, y + 135, 20, pr.RED)
     pr.draw_text(self.timer.best_lap_time.str_format, x, y + 160, 20, pr.WHITE)
